c81b922208760007ab562b750829537817396f1a
Philipp Stekl <philipp.stekl@gmx.net>
2015-03-09 14:42:48 +0100

    added chksum

--- levelk.cc
+++ levelk.cc
@@ -18,237 +18,19 @@ int signal(double q = 0.6) {
 }
 
 
-//new test
-
-// //new testing
-// template <typename T>
-// T prob(boost::dynamic_bitset<>& x, levelkworld<T> lkw){
-//   T tmp=1.;
-
-//   std::vector<int> cases;
-//   std::vector<T> res;
-
-//   for(int i=0; i<x.size(); ++i){
-//     int l2c=-1;
-//     int l3c=-1;
-//     //create copy of bitset up to bit i
-//     boost::dynamic_bitset<> y(i+1);
-//     for(int j=0; j<i+1; ++j){
-//       y[j] = x[j];
-//     }
-//     //check in which state the levelks would be at bit i
-//     //@param cascade =0 NC, 1 AC, 2 BC
-//     level2(y, 0, l2c);
-//     level3(y, 0, l3c);
-
-//     //std::cout<<"l2c: "<<l2c<<std::endl;
-//     //std::cout<<"l3c: "<<l3c<<std::endl;
-
-//     //both A cascade : case 0
-//     if(l2c==1 && l3c==1) {
-//       // A push back prob for A
-//       if(y[i] == 1) {
-//         tmp*= lkw.p[1];
-//         res.push_back(lkw.p[1]);
-//       }
-//       else{ // B push back prob for B
-//         tmp*= (1.-lkw.p[1]);
-//         res.push_back(1.- lkw.p[1]);
-//       }
-//       continue;
-//     }
-//     //both B cascade : case 1
-//     if(l2c==2 && l3c==2) {
-//       //res *= lkw.p[2];
-//       // A push back prob for A
-//       if(y[i] == 1) {
-//         res.push_back(lkw.p[2]);
-//       }
-//       else{ // B push back prob for B
-//         res.push_back(1.- lkw.p[2]);
-//       }
-//       continue;
-//     }
-//     // l2 A, l3 nc : case 2
-//     if(l2c==1 && l3c==0) {
-//       //res *= lkw.p[3];
-//       // A push back prob for A
-//       if(y[i] == 1) {
-//         res.push_back(lkw.p[3]);
-//       }
-//       else{ // B push back prob for B
-//         res.push_back(1.- lkw.p[3]);
-//       }
-//       continue;
-//     }
-//     //l2B, l3 nc : case 3
-//     if(l2c==2 && l3c==0) {
-//       //res *= lkw.p[4];
-//       // A push back prob for A
-//       if(y[i] == 1) {
-//         res.push_back(lkw.p[4]);
-//       }
-//       else{ // B push back prob for B
-//         res.push_back(1.- lkw.p[4]);
-//       }
-//       continue;
-//     }
-//     //l3A, l2 nc : case 4
-//     if(l2c==0 && l3c==1) {
-//       //res *= lkw.p[5];
-//       // A push back prob for A
-//       if(y[i] == 1) {
-//         res.push_back(lkw.p[5]);
-//       }
-//       else{ // B push back prob for B
-//         res.push_back(1.- lkw.p[5]);
-//       }
-//       continue;
-//     }
-//     //l3B, l2nc : case 5
-//     if(l2c==0 && l3c==2) {
-//       //res *= lkw.p[6];
-//       // A push back prob for A
-//       if(y[i] == 1) {
-//         res.push_back(lkw.p[6]);
-//       }
-//       else{ // B push back prob for B
-//         res.push_back(1.- lkw.p[6]);
-//       }
-//       continue;
-//     }
-//     //no cascaed : case 6
-//     if(l2c==0 && l3c==0) {
-//       //res *= lkw.p[0];
-//       // A push back prob for A
-//       if(y[i] == 1) {
-//         res.push_back(lkw.p[0]);
-//       }
-//       else{ // B push back prob for B
-//         res.push_back(1.- lkw.p[0]);
-//       }
-//       continue;
-//     }
-
-//     // l2A l3 B : case 7
-//     if(l2c==1 && l3c==2) {
-//       //res *= lkw.p[0];
-//       // A push back prob for A
-//       if(y[i] == 1) {
-//         res.push_back(lkw.p[7]);
-//       }
-//       else{ // B push back prob for B
-//         res.push_back(1.- lkw.p[7]);
-//       }
-//       continue;
-//     }
-//     // l2B l3 A : case 8
-//     if(l2c==2 && l3c==1) {
-//       //res *= lkw.p[0];
-//       // A push back prob for A
-//       if(y[i] == 1) {
-//         res.push_back(lkw.p[8]);
-//       }
-//       else{ // B push back prob for B
-//         res.push_back(1.- lkw.p[8]);
-//       }
-//       continue;
-//     }
-//     else{
-//       std::cout<<"ERROR: unspecified case in col tab"<<std::endl;
-//     }
-//   }
-
-
-
-
-
-
-
-
-// template <typename T>
-// void tab2nc(T q, levelkworld<T> w, boost::dynamic_bitset<>& x, std::vector<double>& ct, std::vector<double>& res){
-
-//     // p[0] = nc();
-//     // p[1] = l2l3A();
-//     // p[2] = l2l3B();
-//     // p[3] = l2A();
-//     // p[4] = l2B();
-//     // p[5] = l3A();
-//     // p[6] = l3B();
-
-//   double tmp=1.;
-
-//   //check for no cascade
-//   for(int i=0; i < ct.size(); ++i){
-//     tmp=1.;
-//     if( (ct[i] == w.p[0]) || (ct[i] == 1.-w.p[0]) ) {
-//       for(int j=0; j<i+1; ++j){
-//         tmp *= ct[j];
-//       }
-//       res.push_back(tmp);
-//     }
-//     else{
-//       res.push_back(0.);
-//     }
-
-//   }
-
-//   // std::cout<<"Table2 No cascade:"<<std::endl;
-//   // for(std::vector<double>::iterator it = res.begin(); it != res.end(); ++it){
-//   //   std::cout<<*it<<std::endl;
-//   // }
-
-//   return;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   // for(std::vector<double>::iterator it = res.begin(); it != res.end(); ++it){
-//   //   std::cout<<*it<<std::endl;
-//   // }
-//   return 0.;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 template <typename T>
 void table1(T q, levelkworld<T> lkw){
 
-  int n=20;
+  int n=15;
 
 
   //probabilities for the 9 cases
   std::vector<std::vector<double>> pt(9, std::vector<double>(n));
+
+  std::vector<double> chksum(n,0.);
 
 
   //create sequences
@@ -298,9 +80,10 @@ void table1(T q, levelkworld<T> lkw){
       pt[6][i] += tab2l3Bres[i];
       pt[7][i] += tab2l2Al3Bres[i];
       pt[8][i] += tab2l2Bl3Ares[i];
-
-
     }
+
+      for(int k=0; k<9; ++k)
+        chksum[i] += pt[k][i];
   }
 
   for(int j=0; j<pt.size(); ++j){
@@ -310,6 +93,11 @@ void table1(T q, levelkworld<T> lkw){
     }
     std::cout<<"\n";
   }
+  std::cout<<"CHK SUM:"<<std::endl;
+  for(int i=0; i < chksum.size(); ++i){
+    std::cout<<chksum[i]<<"\t";
+  }
+  std::cout<<"\n";
   return;
 }
 
